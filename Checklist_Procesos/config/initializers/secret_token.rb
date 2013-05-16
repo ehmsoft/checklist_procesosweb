6d495a5896c19dd2a3c7f52148e3aa08eda67b93
Andrés Torres <elfotografo007@gmail.com>
2013-05-16 12:17:14 -0500

    Commit Inicial

---
+++ Checklist_Procesos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChecklistProcesos::Application.config.secret_token = '0aa8cf36585f0539b96c1b6750138e3a21c3f1ab73c522d774afc916989a4160115a9d8593982c199f6e4ec3d5c2ae36c1bf1e734d721e3b30082f2c8a4f979f'
