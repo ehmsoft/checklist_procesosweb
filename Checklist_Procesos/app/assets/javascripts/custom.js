27b73aa3ebbda152dc9331a8df8b0b5113802d6a
Andrés Torres <elfotografo007@gmail.com>
2013-08-01 12:50:56 -0500

    Se agregan mejoras a los slider

--- Checklist_Procesos/app/assets/javascripts/custom.js
+++ Checklist_Procesos/app/assets/javascripts/custom.js
@@ -1,3 +1,16 @@
 $(document).bind("mobileinit", function(){
   $.mobile.ajaxEnabled = false;
 });
+
+
+$(document).bind("pageshow", function(){
+	$('#visita_diaria_n_estados').click(function(){
+		this.select();
+	});
+	$('#visita_diaria_n_edictos').click(function(){
+		this.select();
+	});
+	$('#visita_diaria_n_traslados').click(function(){
+		this.select();
+	});
+});
