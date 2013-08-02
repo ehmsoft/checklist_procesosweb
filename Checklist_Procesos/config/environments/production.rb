fc8828749a1bc44f8da7c0b99f854335b0ce8f7a
Andrés Torres <elfotografo007@gmail.com>
2013-08-01 22:44:20 -0500

    Se actualiza la aplicación a Rails 4

--- Checklist_Procesos/config/environments/production.rb
+++ Checklist_Procesos/config/environments/production.rb
@@ -6,8 +6,10 @@ ChecklistProcesos::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
+  
+  #Habilitar Cache
   config.action_controller.perform_caching = true
-
+  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public"
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -64,4 +66,5 @@ ChecklistProcesos::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.eager_load = true
 end
--- Checklist_Procesos/config/initializers/secret_token.rb
+++ Checklist_Procesos/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ChecklistProcesos::Application.config.secret_token = '0aa8cf36585f0539b96c1b6750138e3a21c3f1ab73c522d774afc916989a4160115a9d8593982c199f6e4ec3d5c2ae36c1bf1e734d721e3b30082f2c8a4f979f'
+ChecklistProcesos::Application.config.secret_key_base = 'd142aa845df3b956cb38a4682eb6d702c3d974ff7888a435e0785d601568c63f18dd89695610134c76e9b48f609f382ba1e5c8c8defb972011149e3e480f866e'
